dad83524a68035cc929b03b7f5793aefe1d2a4a9
xyuanmu@gmail.com <x19960620>
2015-10-26 10:22:42 +0800

    提取 ip_tmpok 时去重

--- bindip.py
+++ bindip.py
@@ -1,18 +1,22 @@
 
 import os
 
-iplist = []
-
 if os.path.exists('ip_tmpok.txt'):
+    line_list = []
+    ip_list = []
+    new_line_list = []
     with open('ip_tmpok.txt') as ip_tmpok:
         for x in ip_tmpok:
             sline = x.strip().split(' ')
-            iplist.append(sline)
-            if sline[1].startswith("NA_"):
-                sline[1] = sline[1].lstrip('NA_')
+            if sline[1].startswith("NA_"): sline[1] = sline[1].lstrip('NA_')
+            line_list.append(sline)
+    line_list.sort(key=lambda x: int(x[1]))
+    for line in line_list:
+        if line[0] not in ip_list:
+            ip_list.append(line[0])
+            new_line_list.append(line)
     with open('ip_bind.txt', 'w') as ip_bind:
-        iplist.sort(key=lambda x: int(x[1]))
-        out = '|'.join(x[0] for x in iplist if int(x[1]) < 3000) # 默认抽取3000ms以内IP，可自行修改
+        out = '|'.join(x[0] for x in new_line_list if int(x[1]) < 2000) # 默认抽取2000ms以内IP，可自行修改
         print(out)
         # print(iplist)
         ip_bind.write(out)
